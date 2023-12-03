73c7772a5597a4f679e8e10331d1d488a657ce18
Meithan West <meithan@gmail.com>
2023-12-02 23:48:11 -0600

    Add Julia boilerplate

---
+++ boilerplate.jl
@@ -0,0 +1,26 @@
+# Day XX:
+
+# ==============================================================================
+
+# Get input filename as CLI arg. If not given, default to "day<X>.in"
+if length(ARGS) > 0
+  fname = ARGS[1]
+else
+  fname = replace(PROGRAM_FILE, ".jl" => ".in")
+end
+
+# Parse input
+lines = readlines(fname)
+for line in lines
+
+end
+
+# ------------------------------------------------------------------------------
+# Part 1
+
+println("Part 1: ", nothing)
+
+# ------------------------------------------------------------------------------
+# Part 2
+
+println("Part 2: ", nothing)
